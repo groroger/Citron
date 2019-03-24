011e57552cf2815fb8c55ad5047e2205ffe22782
PuddiPanda <damien.claerhout@gmail.com>
2019-03-24 09:33:54 +0100

    affichage du stock

---
+++ Projet1Parent/Projet1Web/src/main/java/fr/afcepf/al33/projet1/controller/AfficherStockManagedBean.java
@@ -0,0 +1,71 @@
+package fr.afcepf.al33.projet1.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import fr.afcepf.al33.projet1.IBusiness.StockIBusiness;
+import fr.afcepf.al33.projet1.entity.Stock;
+
+
+
+
+
+@ManagedBean(name="mbStock")
+@SessionScoped
+public class AfficherStockManagedBean implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	private List <Stock> stocks;
+	
+	
+	@EJB
+	private StockIBusiness proxyStock;
+	
+	
+
+	@PostConstruct
+	public void init() {
+		stocks=proxyStock.getAll();
+		
+	}
+
+
+
+
+	public List<Stock> getStocks() {
+		return stocks;
+	}
+
+
+
+
+	public void setStocks(List<Stock> stocks) {
+		this.stocks = stocks;
+	}
+
+
+
+
+	public StockIBusiness getProxyStock() {
+		return proxyStock;
+	}
+
+
+
+
+	public void setProxyStock(StockIBusiness proxyStock) {
+		this.proxyStock = proxyStock;
+	}
+
+
+}
