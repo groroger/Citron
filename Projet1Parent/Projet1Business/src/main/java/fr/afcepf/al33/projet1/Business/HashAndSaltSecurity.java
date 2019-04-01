66d586c8aa14a3d0a53019cd52199cb46d8df401
formation <formation@FORM274>
2019-04-01 13:41:12 +0200

    methode hash and salt finie dans le module business (test en console possible en decommentant le main inclus)

--- Projet1Parent/Projet1Business/src/main/java/fr/afcepf/al33/projet1/Business/HashAndSaltSecurity.java
+++ Projet1Parent/Projet1Business/src/main/java/fr/afcepf/al33/projet1/Business/HashAndSaltSecurity.java
@@ -1,21 +1,67 @@
 package fr.afcepf.al33.projet1.Business;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class HashAndSaltSecurity {
-	
-	
-	private static final String ALGO_HASH = "MD5"; 
-	private byte [] salt = generateSalt();
-	
 
-	public static byte[] generateSalt()
+
+	private static String algoHash = "SHA-256"; 
+	private static byte [] salt = genererSalt();
+
+
+	public static byte[] genererSalt()
 	{
 		SecureRandom sr = new SecureRandom();
-		byte[] salt = new byte[3];
+		byte[] salt = new byte[16];
 		sr.nextBytes(salt);
 		return salt;
 	}
+
+	private static String genererHash(String passwordToHash) 
+
+	{
+
+		String passwordCree = null;
+		MessageDigest md;
+		try 
+		{
+			md = MessageDigest.getInstance(algoHash);
 	
+			md.update(salt);
+			byte [] hash = md.digest(passwordToHash.getBytes());
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i< hash.length ;i++)
+			{
+				sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			//Get complete hashed password in hex format
+			passwordCree = sb.toString();
+
+		} 
+		catch (NoSuchAlgorithmException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return passwordCree;
+
+	}
+
+//	TEST EN CONSOLE DU HASH AND SALT 
 	
+//	public static void main(String[] args) {
+//
+//		System.out.println("entrez un mot de passe");
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		String mdp = sc.nextLine();
+//
+//		System.out.println(genererHash(mdp));
+//
+//
+//	}
 }
