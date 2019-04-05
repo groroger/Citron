099495dcf7d21477f504769068660751d5a3e77f
formation <formation@FORM280>
2019-04-05 10:47:40 +0200

    plus and minus in Panier

--- Projet1Parent/Projet1Web/src/main/java/fr/afcepf/al33/projet1/controller/utilisateur/PanierManagedBean.java
+++ Projet1Parent/Projet1Web/src/main/java/fr/afcepf/al33/projet1/controller/utilisateur/PanierManagedBean.java
@@ -1,14 +1,17 @@
 package fr.afcepf.al33.projet1.controller.utilisateur;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
 
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
-
+import fr.afcepf.al33.projet1.entity.Article;
 import fr.afcepf.al33.projet1.entity.ArticleCommande;
 
 
@@ -29,7 +32,62 @@ public class PanierManagedBean implements Serializable{
   
   @ManagedProperty(value="#{mbCatalogueClient.quantiteSaisie}")
   private int quantiteSaisie;
+  
+  private ArticleCommande articleCommande;  
+  
+  public void ajouterArticle(Article article) {
 
+		articleCommande = new ArticleCommande();
+		articleCommande.setArticle(article);
+		articleCommande.setQuantite(article.getQuantiteSaisie());
+		boolean isPresent = false;
+
+
+		if (articlesCommandes.isEmpty()){
+			articlesCommandes.add(articleCommande);
+			System.out.println("ajout premier article");
+		} else {
+
+			Iterator<ArticleCommande> ite = articlesCommandes.iterator();
+
+
+			while(ite.hasNext()) {
+				ArticleCommande ac = ite.next();
+				if (ac.getArticle().getId()==articleCommande.getArticle().getId()) {
+					ac.setQuantite(ac.getQuantite()+ article.getQuantiteSaisie());
+					System.out.println("nombre ajouté à la ligne existante");
+					isPresent = true;
+				}
+			} 
+			
+			if (isPresent == false) {
+				articlesCommandes.add(articleCommande);
+			}
+		}
+		
+		System.out.println("Art Quantite "+articleCommande.getQuantite());
+		
+	
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		session.setAttribute("listeArticlesCommandes", articlesCommandes);
+		facesContext.getApplication()
+					.getNavigationHandler()
+					.handleNavigation(facesContext,null,"/interfaceClient/affichagePanier.xhtml?faces-redirect=true");
+
+}
+	
+	public void plusQuantiteSaisie(Article article){	
+		article.setQuantiteSaisie(article.getQuantiteSaisie()+1);
+}
+
+	public void minusQuantiteSaisie(Article article){
+		if (article.getQuantiteSaisie()>0) {
+			article.setQuantiteSaisie(article.getQuantiteSaisie()-1);
+		}		
+}
+  
+  
   public List<ArticleCommande> getArticlesCommandes() {
     return articlesCommandes;
   }
