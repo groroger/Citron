025fc621657355e2333312017e2afa972b210709
PuddiPanda <damien.claerhout@gmail.com>
2019-04-06 14:06:51 +0200

    retrait imports inutiles

--- Projet1Parent/Projet1InterfaceBusiness/src/main/java/fr/afcepf/al33/projet1/IBusiness/CommandeIBusiness.java
+++ Projet1Parent/Projet1InterfaceBusiness/src/main/java/fr/afcepf/al33/projet1/IBusiness/CommandeIBusiness.java
@@ -2,7 +2,6 @@ package fr.afcepf.al33.projet1.IBusiness;
 
 import java.util.List;
 
-import fr.afcepf.al33.projet1.entity.Client;
 import fr.afcepf.al33.projet1.entity.Commande;
 
 public interface CommandeIBusiness {
