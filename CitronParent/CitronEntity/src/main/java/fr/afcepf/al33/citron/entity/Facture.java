23255efe2dbf5319b9c80813378d8fecd3430466
Amin <me@example.com>
2019-05-14 23:41:14 +0200

    add getter/setter for Facture

--- CitronParent/CitronEntity/src/main/java/fr/afcepf/al33/citron/entity/Facture.java
+++ CitronParent/CitronEntity/src/main/java/fr/afcepf/al33/citron/entity/Facture.java
@@ -19,30 +19,99 @@ import javax.persistence.Table;
 @Table(name = "facture")
 public class Facture implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+	
+		@Id
+		@Column(name = "id", nullable = false)
+		private String id;
+	
+		@Column(name = "date_creation")
+		private Date date_creation;
+	
+		@Column(name = "co2_footprint")
+		private Double co2_footprint;
+	
+		@Column(name = "delivery_price")
+		private Double delivery_price;
+	
+		@ManyToOne
+		@JoinColumn(referencedColumnName = "id")
+		private Client client;
+	
+		@ManyToOne
+		@JoinColumn(referencedColumnName = "id")
+		private Commande commande;
+	
+		// Constructeur vide
+	
+		public Facture() {
+			super();
+	
+		}
+		
+		//Constructeur chargé
+	
+		public Facture(Date date_creation, Double co2_footprint, Double delivery_price, Client client,
+				Commande commande) {
+			super();
+			this.date_creation = date_creation;
+			this.co2_footprint = co2_footprint;
+			this.delivery_price = delivery_price;
+			this.client = client;
+			this.commande = commande;
+		}
 
-	@Id
-	@Column(name = "id", nullable = false)
-	private String id;
-
-	@Column(name = "date_creation")
-	private Date date_creation;
-
-	@Column(name = "co2_footprint")
-	private Double co2_footprint;
-
-	@Column(name = "delivery_price")
-	private Double delivery_price;
-
-	@ManyToOne
-	@JoinColumn(referencedColumnName = "id")
-	private Client client;
-
-	@ManyToOne
-	@JoinColumn(referencedColumnName = "id")
-	private Commande commande;
+		public String getId() {
+			return id;
+		}
+	
+		public void setId(String id) {
+			this.id = id;
+		}
+	
+		public Date getDate_creation() {
+			return date_creation;
+		}
+	
+		public void setDate_creation(Date date_creation) {
+			this.date_creation = date_creation;
+		}
+	
+		public Double getCo2_footprint() {
+			return co2_footprint;
+		}
+	
+		public void setCo2_footprint(Double co2_footprint) {
+			this.co2_footprint = co2_footprint;
+		}
+	
+		public Double getDelivery_price() {
+			return delivery_price;
+		}
+	
+		public void setDelivery_price(Double delivery_price) {
+			this.delivery_price = delivery_price;
+		}
+	
+		public Client getClient() {
+			return client;
+		}
+	
+		public void setClient(Client client) {
+			this.client = client;
+		}
+	
+		public Commande getCommande() {
+			return commande;
+		}
+	
+		public void setCommande(Commande commande) {
+			this.commande = commande;
+		}
+		
+		
 
 }
