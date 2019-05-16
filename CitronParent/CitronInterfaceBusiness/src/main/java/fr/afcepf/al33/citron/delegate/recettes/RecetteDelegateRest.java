3c6b2a3001d9bdb8bb29926dee28ded5f6dae476
Carole <carole.minhondo@free.fr>
2019-05-16 10:57:18 +0200

    Externalisation dans properties du nom de l'URL web Service dans Delegate

--- CitronParent/CitronInterfaceBusiness/src/main/java/fr/afcepf/al33/citron/delegate/recettes/RecetteDelegateRest.java
+++ CitronParent/CitronInterfaceBusiness/src/main/java/fr/afcepf/al33/citron/delegate/recettes/RecetteDelegateRest.java
@@ -2,6 +2,7 @@ package fr.afcepf.al33.citron.delegate.recettes;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -13,7 +14,12 @@ import fr.afcepf.al33.dto.RecetteSelectionnee;
 public class RecetteDelegateRest implements RecetteDelegate {
 
 	private Client jaxrs2client;
-	private String debutUrlRecettePublic="http://localhost:9393/springBootWebService/rest/public/recettes";
+	
+	// l'URL du Web service est une variable à récupérer dans la fichier application.properties
+	ResourceBundle ressources = ResourceBundle.getBundle("application") ; // accès à application.properties 
+	private String debutUrlRecettePublic = ressources.getString("urlWS");  
+	 	
+	//private String debutUrlRecettePublic="http://localhost:9393/springBootWebService/rest/public/recettes"; ancienne version: nom url en dur
 	public RecetteDelegateRest() {
 		this.jaxrs2client = ClientBuilder.newClient();
 				           /* .register(JacksonFeature.class); */
